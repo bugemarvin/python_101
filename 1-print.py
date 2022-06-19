06d463bd8120ef08d4c8460a53ed4f0710ac2de6
Marvin Kurland <bugemarvin@outlook.com>
2022-06-19 22:11:54 +0300

    printing in python

---
+++ 1-print.py
@@ -0,0 +1,10 @@
+from datetime import date
+# printing - printing statement in python.
+#
+# Return: Always 1 on success.
+
+def printing():
+	today = date.today()
+	print("The Day i stated working on python Dated:", today)
+	print("\"Hi\" am Marvin kurland commited in learning python")
+printing()
